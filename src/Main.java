c2f502a6fcec436cb58a4ad2e144aec31968aacc
DanteSwagKid69 <dante.vonmatern@elev.ga.ntig.se>
2024-05-27 15:44:18 +0200

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.lang.foreign.MemoryLayout;
+import java.net.MulticastSocket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+
+        // Default port number we are going to use
+        int portnumber = 50000;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        // Create a MultitaskSocket
+        MulticastSocket chatMultitaskSocket = new MulticastSocket(portnumber);
+
     }
 }
